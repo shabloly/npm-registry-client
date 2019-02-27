89058c7a4e33b6bd21692e4be58130eddb7a7368
Roy <rois0310@gmail.com>
2019-02-27 18:17:00 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,78 +1,32 @@
-import React, { Component } from 'react';
-import axios from "axios";
-import './App.css';
-import Tree from 'react-animated-tree';
-import testdata from './Data/testdata.json';
-
-import UserForm from "./components/UserForm";
+import React, { Component } from "react";
+import "./App.css";
+import testdata from "./Data/testdata.json";
+import Dependency from "./Dependency";
+import Search from "./components/Search";
 
 class App extends Component {
-
   state = {
-    name:String,
-    version:String,
-    dependencies: []
-  }
+    name: "express",
+    version: "latest"
+  };
 
-  //use test data
-  componentDidMount() {
-    const data = testdata;
-    this.setState({name:'express',version:'latest',dependencies:data.dependencies});
-  }
-
-  getDependecies = async (e) => {
-    e.preventDefault();
-    const moduleName = e.target.elements.modulename.value.trim();
-    const moduleVersion = e.target.elements.moduleversion.value.trim();
-
-    if (moduleName && moduleVersion) {
-      let url = 'http://localhost:8000/getModuleDependencies/'+ moduleName +'/'+ moduleVersion;
-      const res =  await axios.get(url);
-        const depCount = res.data.count;
-        const dependencies = res.data.dependencies;
-
-        if(depCount > 0)
-          this.setState({ dependencies :dependencies });
-        else
-          this.setState({ dependencies :[] });
-
-        this.setState({name:moduleName,version:moduleVersion});
-      }
-    else return;
-    }
+  handleNewSearch = ({ name, version }) => {
+    this.setState({ name, version });
+  };
 
   render() {
-    const treeData = this.state;
-    let depList;
-    if(treeData.dependencies.length > 0) {
-      depList =
-          <Tree key='main'  content={treeData.name + ' ' + treeData.version}  >
-            {
-              treeData.dependencies.map((key,index) => (
-                <Tree key={index}
-                    content={key[0] + ' ' + key[1]}
-                      style={{color: 'blue',  verticalAlign: 'left'}}
-                />
-              ))
-            }
-          </ Tree>;
-    }
-    else {
-      depList = <h3>No Dependencies Found</h3>
-    }
-
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Search Module dependencies</h1>
         </header>
-        <UserForm getDependecies ={this.getDependecies} />
-        <div id='tree'>
-          {depList}
+        <Search onSearch={this.handleNewSearch} />
+        <div id="tree" >
+          <Dependency module={this.state} />
         </div>
       </div>
     );
   }
-};
+}
 
 export default App;
